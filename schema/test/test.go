68f6e3508ae984bb8390827bb8105eaf5357eb90
Randy Reddig <randy@domainr.com>
2021-12-11 11:39:02 -0800

    schema/test: fix DeepEqual part of this test

--- schema/test/test.go
+++ schema/test/test.go
@@ -10,28 +10,28 @@ import (
 
 // RoundTrip validates if v marshals to want or wantErr (if set),
 // and the resulting XML unmarshals to v.
-func RoundTrip(t *testing.T, v interface{}, want string, wantErr bool) {
-	x, err := xml.Marshal(v)
+func RoundTrip(t *testing.T, v interface{}, wantXML string, wantErr bool) {
+	gotXML, err := xml.Marshal(v)
 	if (err != nil) != wantErr {
 		t.Errorf("xml.Marshal() error = %v, wantErr %v", err, wantErr)
 		return
 	}
-	if string(x) != want {
-		t.Errorf("xml.Marshal()\nGot:  %v\nWant: %v", string(x), want)
+	if string(gotXML) != wantXML {
+		t.Errorf("xml.Marshal()\nGot:  %v\nWant: %v", string(gotXML), wantXML)
 	}
 
 	if v == nil {
 		return
 	}
 
-	i := reflect.New(reflect.TypeOf(v).Elem()).Interface()
-	err = xml.Unmarshal(x, i)
+	got := reflect.New(reflect.TypeOf(v).Elem()).Interface()
+	err = xml.Unmarshal(gotXML, got)
 	if err != nil {
 		t.Errorf("xml.Unmarshal() error = %v", err)
 		return
 	}
-	if !reflect.DeepEqual(v, v) {
-		t.Errorf("xml.Unmarshal()\nGot:  %#v\nWant: %#v", i, v)
+	if !reflect.DeepEqual(v, got) {
+		t.Errorf("xml.Unmarshal()\nGot:  %#v\nWant: %#v", got, v)
 	}
 }
 
