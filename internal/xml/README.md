e34bac8dda0cdb92035dcbcefc322fde68ac01ca
Randy Reddig <randy@domainr.com>
2021-12-13 13:57:32 -0800

    internal/xml: proxy package to use stdlib encoding/xml or github.com/nbio/xml fork
    Build with -tags=stdxml to use the standard library encoding/xml package

---
+++ internal/xml/README.md
@@ -0,0 +1,3 @@
+# xml
+
+Package `xml` is a proxy between the `epp` module and the fork of `encoding/xml` it uses to marshal and unmarshal EPP XML into Go types.
---
+++ internal/xml/xml.go
@@ -0,0 +1,18 @@
+//go:build !stdxml
+// +build !stdxml
+
+package xml
+
+import "github.com/nbio/xml"
+
+type Attr = xml.Attr
+type Name = xml.Name
+type StartElement = xml.StartElement
+type EndElement = xml.EndElement
+type Encoder = xml.Encoder
+type Decoder = xml.Decoder
+
+var NewEncoder = xml.NewEncoder
+var NewDecoder = xml.NewDecoder
+var Marshal = xml.Marshal
+var Unmarshal = xml.Unmarshal
