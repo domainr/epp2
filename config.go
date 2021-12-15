0801ad32e028bceb28aeb9755671c243532c943e
Randy Reddig <randy@domainr.com>
2021-12-15 13:50:41 -0800

    update Config documentation; Forced -> Unannounced

--- config.go
+++ config.go
@@ -4,45 +4,59 @@ import (
 	"github.com/domainr/epp2/schema/epp"
 )
 
-// Config describes an EPP client or server configuration, including
-// EPP objects and extensions used for a connection.
+// Config describes an EPP client or server configuration, including EPP objects
+// and extensions used for a connection.
 type Config struct {
-	// Supported EPP version(s). Typically this should not be set
-	// by either a client or server. If nil, this will default to
-	// []string{"1.0"} (currently the only supported version).
+	// Supported EPP version(s). Typically this should not be set by either
+	// a client or server. If nil, this will default to []string{"1.0"}
+	// (currently the only supported version).
 	Versions []string
 
-	// BCP 47 language code(s) for human-readable messages.
-	// For clients, this describes the desired language(s) in preferred order.
-	// If the server does not support any of the client’s preferred languages,
-	// the first language advertised by the server will be selected.
-	// For servers, this describes its supported language(s).
-	// If nil, []string{"en"} will be used.
+	// BCP 47 language code(s) for human-readable messages. For clients,
+	// this describes the desired language(s) in preferred order. If the
+	// server does not support any of the client’s preferred languages, the
+	// first language advertised by the server will be selected. For
+	// servers, this describes its supported language(s). If nil,
+	// []string{"en"} will be used.
 	Languages []string
 
-	// Namespace URIs of EPP objects supported by a client or server.
-	// For clients, this describes the object type(s) the client wants to access.
-	// For servers, this describes the object type(s) the server allows clients to access.
+	// Objects is a list of XML namespace URIs enumerating the EPP objects
+	// supported by the client or server.
+	//
+	// For clients, this describes the object type(s) the client wants to
+	// access.
+	//
+	// For servers, this describes the object type(s) the server allows
+	// clients to access.
+	//
 	// If nil, a reasonable set of defaults will be used.
 	Objects []string
 
-	// EPP extension URIs supported by a client or server.
-	// For clients, this is a list of extensions(s) the client wants to use in preferred order.
-	// If nil, a client will use the highest version of each supported extension advertised by the server.
-	// For servers, this is an advertised list of supported extension(s).
-	// If nil, a server will use a reasonable set of defaults.
+	// Extensions is a list of XML namespace URIs enumerating the EPP
+	// extensions supported by the client or server.
+	//
+	// For clients, this is a list of extensions(s) the client wants to use
+	// in preferred order. If nil, a client will use its preferred version
+	// of each supported extension announced by the server.
+	//
+	// For servers, this is list of announced extension(s).
+	//
+	// If nil, no EPP extensions will be used.
 	Extensions []string
 
-	// ForcedExtensions contains one or more EPP extension URIs to be used
-	// by a client or server, whether or not the peer indicates support for
-	// it. This is used as a workaround for EPP servers that incorrectly
-	// advertise the extensions they support. This value should typically be
-	// left nil. This will always be nil when read from a peer.
-	ForcedExtensions []string
+	// UnannouncedExtensions contains one or more EPP extensions to be used even if
+	// the peer does not announce support for it. This is used as a
+	// workaround for EPP servers that incorrectly announce the extensions
+	// they support.
+	//
+	// This value should typically be left nil. This will always be nil when
+	// read from a peer.
+	UnannouncedExtensions []string
 
-	// TransactionID, if not nil, returns unique values used for client or server transaction IDs.
-	// If nil, a sequential transaction ID with a random prefix will be used.
-	// The function must be safe to call from multiple goroutines.
+	// TransactionID, if not nil, returns unique values used for client or
+	// server transaction IDs. If nil, a sequential transaction ID with a
+	// random prefix will be used. The function must be safe to call from
+	// multiple goroutines.
 	TransactionID func() string
 }
 
@@ -66,7 +80,7 @@ func (c Config) Copy() Config {
 	c.Languages = copySlice(c.Languages)
 	c.Objects = copySlice(c.Objects)
 	c.Extensions = copySlice(c.Extensions)
-	c.ForcedExtensions = copySlice(c.ForcedExtensions)
+	c.UnannouncedExtensions = copySlice(c.UnannouncedExtensions)
 	return c
 }
 
