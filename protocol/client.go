571bf9e534581410c5db14b7309b85a3c473db95
Randy Reddig <randy.reddig@fastly.com>
2023-11-10 17:23:49 -0800

    protocol: EPP servers MUST process commands in the order received
    Replies to commands MUST also be in the order received.
    
    Clients MUST NOT send any command until they have received a <greeting> element from the server.

--- protocol/client.go
+++ protocol/client.go
@@ -2,7 +2,6 @@ package protocol
 
 import (
 	"context"
-	"sync"
 	"sync/atomic"
 	"time"
 
@@ -39,10 +38,7 @@ type client struct {
 	// hasGreeting is closed when the client receives an initial <greeting> from the server.
 	hasGreeting chan struct{}
 
-	syncReplies chan reply
-
-	mu           sync.Mutex
-	transactions map[string]transaction
+	transactions chan transaction
 }
 
 // NewClient returns a new EPP client using conn.
@@ -51,7 +47,7 @@ type client struct {
 func NewClient(conn Conn, schemas ...schema.Schema) Client {
 	c := newClient(conn, schemas)
 	// Read the initial <greeting> from the server.
-	go c.readEPP(context.Background())
+	go c.readEPP()
 	return c
 }
 
@@ -60,12 +56,15 @@ func newClient(conn Conn, schemas schema.Schemas) *client {
 		schemas = DefaultSchemas()
 	}
 	return &client{
-		conn:        conn,
-		schemas:     schemas,
-		hasGreeting: make(chan struct{}),
-		syncReplies: make(chan reply),
+		conn:         conn,
+		schemas:      schemas,
+		hasGreeting:  make(chan struct{}),
+		transactions: make(chan transaction, maxConcurrentTransactions),
 	}
 }
+
+// FIXME: make this an option
+const maxConcurrentTransactions = 16
 
 // Close closes the connection and cancels any pending commands.
 func (c *client) Close() error {
@@ -117,41 +116,6 @@ func (c *client) ServerTime(ctx context.Context) (time.Time, error) {
 	return g.ServerDate.Time, nil
 }
 
-// Exchange sends [epp.Body] req and returns the response from the server.
-// It blocks until a response is received, ctx is canceled, or
-// the underlying connection is closed.
-func (c *client) Exchange(ctx context.Context, req epp.Body) (epp.Body, error) {
-	replies := c.syncReplies
-	if cmd, ok := req.(*epp.Command); ok {
-		if cmd.ClientTransactionID != "" {
-			tx, cancel := newTransaction(ctx)
-			defer cancel()
-			err := c.pushCommand(cmd.ClientTransactionID, tx)
-			if err != nil {
-				return nil, err
-			}
-			replies = tx.reply
-		}
-	}
-
-	err := c.conn.WriteEPP(req)
-	if err != nil {
-		return nil, err
-	}
-
-	err = c.readEPP(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	select {
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	case r := <-replies:
-		return r.body, r.err
-	}
-}
-
 // Greeting returns the last <greeting> received from the server.
 // It blocks until the <greeting> is received, ctx is canceled, or
 // the underlying connection is closed.
@@ -168,32 +132,66 @@ func (c *client) Greeting(ctx context.Context) (*epp.Greeting, error) {
 	}
 }
 
-// readEPP reads a single EPP data unit and dispatches it to an
-// awaiting transaction.
-func (c *client) readEPP(ctx context.Context) error {
-	body, err := c.conn.ReadEPP()
-	if err != nil {
-		return err
+// Exchange sends [epp.Body] req and returns the response from the server.
+// It blocks until a response is received, ctx is canceled, or
+// the underlying connection is closed.
+func (c *client) Exchange(ctx context.Context, req epp.Body) (epp.Body, error) {
+	// Client MUST wait until it has received a <greeting> element from the server
+	// before sending any commands.
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case <-c.hasGreeting:
 	}
-	return c.handleReply(ctx, body)
+
+	tx, cancel := newTransaction(ctx)
+	defer cancel()
+
+	// Queue the transaction.
+	c.transactions <- tx
+
+	// Write one request to the EPP connection.
+	err := c.conn.WriteEPP(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Read one response off the EPP connection, which might not be a response to req.
+	res, err := c.readEPP()
+	if err != nil {
+		return nil, err
+	}
+
+	// Dequeue a transaction.
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case rtx := <-c.transactions:
+		if rtx == tx {
+			return res, nil // Short-circuit
+		}
+		// rtx.result is 1-buffered so this should never block.
+		// If it blocks, then there is a bug in the implementation.
+		rtx.result <- result{res, nil}
+	}
+
+	// Wait for the response to tx.
+	select {
+	case <-tx.ctx.Done():
+		return nil, tx.ctx.Err()
+	case res := <-tx.result:
+		return res.body, res.err
+	}
 }
 
-func (c *client) handleReply(ctx context.Context, body epp.Body) error {
-	replies := c.syncReplies
-	switch body := body.(type) {
-	case *epp.Response:
-		id := body.TransactionID.Client
-		if id != "" {
-			tx, ok := c.popCommand(id)
-			if !ok {
-				// TODO: log when server responds with unknown transaction ID.
-				// TODO: keep abandoned transactions around for some period of time.
-				return TransactionIDError{id}
-			}
-			ctx = tx.ctx // TODO: should we track both contexts?
-			replies = tx.reply
-		}
+func (c *client) readEPP() (epp.Body, error) {
+	// Read one reply, which may not be a reply to req.
+	body, err := c.conn.ReadEPP()
+	if err != nil {
+		return nil, err
+	}
 
+	switch body := body.(type) {
 	case *epp.Greeting:
 		// Always store the last <greeting> received from the server.
 		c.greeting.Store(body)
@@ -203,59 +201,21 @@ func (c *client) handleReply(ctx context.Context, body epp.Body) error {
 		case <-c.hasGreeting:
 		default:
 			close(c.hasGreeting)
-			// Return immediately rather than send to blocked replies channel.
-			return nil
 		}
 	}
 
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case replies <- reply{body: body, err: nil}:
-	}
-
-	return nil
+	return body, err
 }
 
-// pushCommand adds a <command> transaction to the map of in-flight commands.
-func (c *client) pushCommand(id string, tx transaction) error {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	_, ok := c.transactions[id]
-	if ok {
-		return DuplicateTransactionIDError{id}
-	}
-	if c.transactions == nil {
-		c.transactions = make(map[string]transaction)
-	}
-	c.transactions[id] = tx
-	return nil
-}
-
-// popCommand removes a <command> transaction from the map of in-flight commands.
-func (c *client) popCommand(id string) (transaction, bool) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	tx, ok := c.transactions[id]
-	if ok {
-		delete(c.transactions, id)
-	}
-	return tx, ok
-}
-
-// cleanup cleans up and responds to all in-flight <hello> and <command> transactions.
+// cleanup cleans up and responds to all in-flight transactions.
 // Each transaction will be finalized with err, which may be nil.
 //
 // TODO: clean up stale or abandoned transactions on a regular basis?
 func (c *client) cleanup(err error) {
-	c.mu.Lock()
-	commands := c.transactions
-	c.transactions = nil
-	c.mu.Unlock()
-	for _, tx := range commands {
+	for tx := range c.transactions {
 		select {
 		case <-tx.ctx.Done():
-		case tx.reply <- reply{err: err}:
+		case tx.result <- result{err: err}:
 		}
 	}
 }
--- protocol/transaction.go
+++ protocol/transaction.go
@@ -7,19 +7,19 @@ import (
 )
 
 type transaction struct {
-	ctx   context.Context
-	reply chan reply
+	ctx    context.Context
+	result chan result
 }
 
 func newTransaction(ctx context.Context) (transaction, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(ctx)
 	return transaction{
-		ctx:   ctx,
-		reply: make(chan reply, 1), // 1-buffered to not block
+		ctx:    ctx,
+		result: make(chan result, 1), // 1-buffered to not block
 	}, cancel
 }
 
-type reply struct {
+type result struct {
 	body epp.Body
 	err  error
 }
