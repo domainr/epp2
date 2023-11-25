f1cc876292448ebafc1e4bc4d889577a445277e2
Randy Reddig <randy@domainr.com>
2023-11-25 13:18:43 -0800

    protocol: improve server tests

--- protocol/dataunit/server_test.go
+++ protocol/dataunit/server_test.go
@@ -1,36 +1,47 @@
 package dataunit
 
 import (
-	"sync"
 	"testing"
 )
 
 func TestServer(t *testing.T) {
 	clientConn, serverConn := Pipe()
-
-	var wg sync.WaitGroup
-	defer wg.Wait()
-	wg.Add(1)
-	go func() {
-		clientConn.WriteDataUnit([]byte("hello"))
-		res, err := clientConn.ReadDataUnit()
-		if err != nil {
-			t.Errorf("ReadDataUnit(): err == %v", err)
-		}
-		if string(res) != "world" {
-			t.Errorf("ReadDataUnit(): got %s, expected %s", string(res), "world")
-		}
-		wg.Done()
-	}()
+	defer clientConn.Close()
+	defer serverConn.Close()
 
 	s := NewServer(serverConn, 1)
+	go echoServer(t, s)
 
-	req, w, err := s.Next()
+	err := clientConn.WriteDataUnit([]byte("hello"))
 	if err != nil {
-		t.Errorf("Next(): err == %v", err)
+		t.Errorf("WriteDataUnit(): err == %v", err)
 	}
-	if string(req) != "hello" {
-		t.Errorf("Next(): got %s, expected %s", string(req), "hello")
+	res, err := clientConn.ReadDataUnit()
+	if err != nil {
+		t.Errorf("ReadDataUnit(): err == %v", err)
 	}
-	err = w.WriteDataUnit([]byte("world"))
+	if string(res) != "hello" {
+		t.Errorf("ReadDataUnit(): got %s, expected %s", string(res), "world")
+	}
+}
+
+// echoServer implements a rudimentary EPP data unit server that echoes
+// back each received request.
+func echoServer(t *testing.T, s Server) {
+	for {
+		if t.Failed() {
+			return
+		}
+		req, w, err := s.Next()
+		if err != nil {
+			t.Errorf("echoServer: Next(): err == %v", err)
+			return
+		}
+		err = w.WriteDataUnit(req)
+		println("replied")
+		if err != nil {
+			t.Errorf("echoServer: WriteDataUnit(): err == %v", err)
+			return
+		}
+	}
 }
