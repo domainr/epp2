1094b340a397c7eebd23fa4b9e3b017d8ee63895
Randy Reddig <randy.reddig@fastly.com>
2023-12-05 09:49:55 -0800

    protocol: typo (PR feedback)

--- protocol/server.go
+++ protocol/server.go
@@ -29,7 +29,7 @@ func (f responderFunc) RespondEPP(ctx context.Context, body epp.Body) error {
 //
 // [RFC 5730]: https://datatracker.ietf.org/doc/rfc5730/
 type Server interface {
-	// ServeEPP provides an client EPP request and a mechanism to respond to the request.
+	// ServeEPP provides an EPP request and a mechanism to respond to the request.
 	// It blocks until a response is received, Context is canceled, or the underlying connection is closed.
 	//
 	// The supplied Context must be non-nil, and only affects reading the request from the client.
